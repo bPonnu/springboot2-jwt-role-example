22f19475a467706431a4baa2294dc5e76362318f
Biraveenmaks Ponnu <biraveenmaks.ponnu@zuehlke.com>
2019-04-25 13:39:57 +0200

    initial commit

---
+++ src/main/java/com/example/polls/controller/UserController.java
@@ -0,0 +1,97 @@
+package com.example.polls.controller;
+
+import com.example.polls.exception.AppException;
+import com.example.polls.exception.ResourceNotFoundException;
+import com.example.polls.model.Role;
+import com.example.polls.model.RoleName;
+import com.example.polls.model.User;
+import com.example.polls.payload.*;
+import com.example.polls.repository.RoleRepository;
+import com.example.polls.repository.UserRepository;
+import com.example.polls.security.UserPrincipal;
+import com.example.polls.security.CurrentUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    //@Secured("USER")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+        return userSummary;
+    }
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/users/{username}")
+    public UserProfile getUserProfile(@PathVariable(value = "username") String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+
+        UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt());
+
+        return userProfile;
+    }
+
+    //TODO
+    @GetMapping("/user/makemeadmin")
+    public String makeToAdmin(@CurrentUser UserPrincipal currentUser){
+        User user = userRepository.findByUsername(currentUser.getUsername())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", currentUser.getUsername()));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
+                .orElseThrow(() -> new AppException("User Role not set."));
+
+        user.setRoles(null);
+        user.setRoles(Collections.singleton(userRole));
+
+        User result = userRepository.saveAndFlush(user);
+
+        Set<Role> roles= result.getRoles();
+        Iterator<Role> iterator = roles.iterator();
+        Role first = iterator.next();
+        String roleName = first.getName().name();
+
+        //String roleName = result.getRoles().stream().findFirst().get().getName().name();
+
+        return "Now admin: " + roleName;
+        //return "hello";
+    }
+
+
+
+}
